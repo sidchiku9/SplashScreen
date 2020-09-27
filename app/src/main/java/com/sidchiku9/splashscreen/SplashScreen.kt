c7c9d702821f3bd21a2837464a41e5d030e23f60
sidchiku9 <sidchiku9@gmail.com>
2020-09-27 21:29:45 +0530

    Initial commit

---
+++ app/src/main/java/com/sidchiku9/splashscreen/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.sidchiku9.splashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+
+class SplashScreen : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT : Long = 3000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
